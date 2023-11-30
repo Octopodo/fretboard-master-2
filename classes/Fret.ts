b68eacea5da476edceed99fcb586beb32133975e
Oktopoid <orson_wells40@hotmail.com>
2023-11-30 18:45:48 +0100

    .

--- classes/Fret.ts
+++ classes/Fret.ts
@@ -1,12 +1,12 @@
 class Fret {
-  private active: Boolean = true;
-  private color: any = null;
-  private decoration: any = null;
-  private finger: Number = 0;
-  private index: Number = 0;
-  private octave: Number = 4;
-  private offset: Boolean = false;
-  private pitch: String = "C";
+  private active: Boolean = true
+  private color: any = null
+  private decoration: any = null
+  private finger: Number = 0
+  private index: Number = 0
+  private octave: Number = 4
+  private offset: Boolean = false
+  private pitch: String = 'C'
 
   constructor(
     active?: Boolean,
