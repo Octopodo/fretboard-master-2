dcbfaac33a46bd1d0f86d731426ab794e05ebb41
Oktopoid <orson_wells40@hotmail.com>
2023-11-30 15:42:37 +0100

    createv base fret view component

---
+++ components/fretboard/fret/view.vue
@@ -0,0 +1,36 @@
+<template>
+  <div
+  :style="fretStyle"
+  class="place-content-center"
+  @click.prevent="toogleSelection"
+  @contextmenu.prevent="toggleStyleMenu"
+  >
+  <transition name="scale-transition">
+    <div
+      v-if="visible"
+      :style="[toneStyle]"
+      class="place-content-center"
+    >
+    <div class="unselectable">
+      {{ displayTone }}
+    </div>
+
+    </div>
+  </transition>
+    
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+const store = useFretStore()
+const visible = computed(()=>false)
+const displayTone = computed(() => 'A3')
+const decoration = computed(() => false)
+const ghosted = computed(() => false)
+const color = computed(() => '')
+</script>
+
+<style>
+
+</style>
