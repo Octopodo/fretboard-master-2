e6e0fd343713d270579da47c937fb482e2e30633
Miguel de Mendoza <orson_wells40@hotmail.com>
2023-12-01 10:37:32 +0100

    Testing

--- app.vue
+++ app.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <fretboard-fret-view></fretboard-fret-view>
-    <fretboard-fret-view></fretboard-fret-view>
+    <!-- <fretboard-fret-view></fretboard-fret-view> -->
+    <fretboard-fret-view :width="60"></fretboard-fret-view>
     <fretboard-fret-view></fretboard-fret-view>
     <fretboard-fret-view></fretboard-fret-view>
   </div>
