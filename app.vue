d2bfa4d9ed001c3d76533d9934b9c38a499c29dc
Miguel de Mendoza <orson_wells40@hotmail.com>
2023-12-02 02:14:02 +0100

    .

--- app.vue
+++ app.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <!-- <fretboard-fret-view></fretboard-fret-view> -->
-    <fretboard-fret-view :width="60"></fretboard-fret-view>
-    <fretboard-fret-view></fretboard-fret-view>
+    <!-- <fretboard-fret-view :width="60"></fretboard-fret-view>
+    <fretboard-fret-view></fretboard-fret-view> -->
     <fretboard-fret-view></fretboard-fret-view>
   </div>
 </template>
