ed60dd24a4e0efc388b30296164a6bb64aeb5a48
Miguel de Mendoza <orson_wells40@hotmail.com>
2023-12-03 00:50:25 +0100

    File created

---
+++ components/fretboard/fret/img-layers.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+
+// PROPS
+const props = defineProps({
+  src: {type: String, default: ''},
+  path: {type: String, default: ''},
+})
+
+// METHODS
+const getPaths = () => {
+  
+}
+
+
+</script>
+
+<style>
+
+</style>
+
