e0f16756cf48403f91eab16235cd2026bf530ecb
Miguel de Mendoza <orson_wells40@hotmail.com>
2023-12-02 20:28:57 +0100

    Added sass transitions path

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+const styleFiles = (path: String) => {
+  const fs = require('fs')
+  const files = fs.readdirSync(path)
+  return files.map((i:any) => path + i)
+}
+
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: [
@@ -14,5 +20,8 @@ export default defineNuxtConfig({
       'composables/**', 
       'constants/**'],
   },
-  css: ['~/assets/styles/main.sass'],
+  css: [
+    '~/assets/styles/main.sass',
+    '~/assets/styles/transitions.sass'
+  ],
 })
