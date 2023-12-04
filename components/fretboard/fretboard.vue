b5d5afa07455c0e13718b60ff46f84181e70b6dd
Miguel de Mendoza <orson_wells40@hotmail.com>
2023-12-04 20:38:36 +0100

    Frist fretboard implementation

--- components/fretboard/fretboard.vue
+++ components/fretboard/fretboard.vue
@@ -1,21 +1,52 @@
 <template>
-  <div>
-    <div v-for="gString in 6">
-      a
+  <div class="fretboard" :style="stringTemplate">
+    <div class="string" :style="fretTemplate" v-for="str in fretboard.matrix" >
+      <fretboard-fret-view  v-for="fret in str" :fret-data="fret"></fretboard-fret-view>
     </div>
   </div>
-string/temstrings>
+
+</template>
 
 <script lang="ts" setup>
-import { useMyFretboardStore } from '~/stores/FretboardStore';
+import { useMyFretboardStore } from '~/stores/FretboardStore'
+import {  fretSpacing } from '~/constants/fret-proportions'
+import { Fretboard } from '~/classes/Fretboard'
 
-const frets = ref(22)
-const stringCount = computed(() => {return 6})
+const props = defineProps({
+  strings: { type: [Number, String], default: 6 },
+  frets: { type: [Number, String], default: 12}
+})
 
+
+const fretCount = computed(() => Number(props.frets))
+const stringCount = computed(() => Number(props.strings))
 const state = useMyFretboardStore()
+const fretboard = computed(() => new Fretboard(stringCount.value, fretCount.value))
+
+fretboard.value.createMatrix(stringCount.value, fretCount.value)
+
+
+const fretTemplate = computed(()=> {
+  let fretSpacingCss = fretSpacing.map(space => `${space}fr`).join(' ')
+  return {
+    gridTemplateColumns: fretSpacingCss,
+    gidTemplateRows: 'auto'
+  }
+})
+
+const stringTemplate = computed(() => {
+  return {
+    gridTemplateColumns: 'auto',
+    gidTemplateRows: `repeat(${stringCount}, 1fr)`
+  }
+})
+
+onBeforeMount(() => {
+  
+})
 </script>
 
-<style>
+<style lang="sass">
 
 </style>
 
