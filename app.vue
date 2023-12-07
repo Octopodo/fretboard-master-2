eab6a791a8c0378fb68aa62a4eaadd57ea40e438
Oktopoid <orson_wells40@hotmail.com>
2023-12-07 18:34:44 +0100

    Testing changes

--- app.vue
+++ app.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- <div class="fretboard-string"> -->
-  <testing-fretboard-model></testing-fretboard-model>
-  <!-- <fretboard frets="24"></fretboard> -->
+  <!-- <testing-fretboard-model></testing-fretboard-model> -->
+  <fretboard frets="24"></fretboard>
   <!-- <fretboard-fret-view class="" v-for="fret in 20"></fretboard-fret-view> -->
   <!-- </div> -->
 </template>
--- models/scales/ScaleHelpers.ts
+++ models/scales/ScaleHelpers.ts
@@ -1,6 +1,8 @@
 import { type Scale as ScaleInterface } from '@tonaljs/scale'
 
-export const isMinor = (scale: ScaleInterface) => {
+const isMinor = (scale: ScaleInterface) => {
   let isMinor = scale.intervals.indexOf('3m') !== -1
   return isMinor
 }
+
+export { isMinor }
