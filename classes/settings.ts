1225b6e3d69468a4a5d76c8d2ce66d69e411a625
Oktopoid <orson_wells40@hotmail.com>
2023-12-07 12:02:57 +0100

    Added Fretboard base settings

--- classes/settings.ts
+++ classes/settings.ts
@@ -1,10 +1,16 @@
+import { Tunings } from '~/constants/musical'
 export const FretboardSettings = {
+  FLATS: false,
+  FRET_COUNT: 24,
+  MAX_HEIGHT: 350,
   MAX_WIDTH: 0,
-  MAX_HEIGHT: 350
+  SCALE: 'major',
+  STRING_COUNT: 6,
+  TONE: 'C1',
+  TUNNING: Tunings.STANDARD,
 }
 
-
 export const FretSettings = {
+  FRET_DOT_SIZE: 30,
   VISIBLE_AT_START: true,
-  FRET_DOT_SIZE: 30
 }
